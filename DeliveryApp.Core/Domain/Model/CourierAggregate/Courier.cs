5f14fe934992fa24b01897d98700707d7479084e
Dmitriy Vasilev <dmitriy.vasilev.dev@gmail.com>
2025-03-13 00:46:26 +0300

    added aggregates

---
+++ DeliveryApp.Core/Domain/Model/CourierAggregate/Courier.cs
@@ -0,0 +1,72 @@
+using CSharpFunctionalExtensions;
+using DeliveryApp.Core.Domain.Model.SharedKernel;
+using Primitives;
+
+namespace DeliveryApp.Core.Domain.Model.CourierAggregate;
+
+public class Courier : Aggregate<Guid>
+{
+    public string Name { get; private set; }
+    public Transport Transport { get; private set; }
+    public Location Location { get; private set; }
+    public CourierStatus Status { get; private set; }
+
+    private Courier(string name, Transport transport, Location location) : base(Guid.NewGuid())
+    {
+        Name = name;
+        Transport = transport;
+        Location = location;
+        Status = CourierStatus.Free;
+    }
+
+    public static Result<Courier, Error> Create(
+        string name,
+        string transportName,
+        int transportSpeed,
+        Location location)
+    {
+        if (string.IsNullOrWhiteSpace(name)) return GeneralErrors.ValueIsRequired(nameof(name));
+        if (location is null) return GeneralErrors.ValueIsRequired(nameof(location));
+
+        var (_, isFailure, transport, error) = Transport.Create(transportName, transportSpeed);
+
+        if (isFailure) return error;
+        
+        return new Courier(name, transport, location);
+    }
+
+    public UnitResult<Error> SetBusy()
+    {
+        if (Status == CourierStatus.Busy) return Errors.CourierIsBusy;
+        
+        Status = CourierStatus.Busy;
+
+        return UnitResult.Success<Error>();
+    }
+
+    public void SetFree()
+    {
+        Status = CourierStatus.Free;
+    }
+
+    public UnitResult<Error> Move(Location destination)
+    {
+        if (destination is null) return GeneralErrors.ValueIsRequired(nameof(destination));
+        
+        var (_, isFailure, newLocation, error) = Transport.Move(Location, destination);
+        
+        if (isFailure) return error;
+        
+        Location = newLocation;
+        
+        return UnitResult.Success<Error>();
+    }
+}
+
+public static class Errors
+{
+    public static Error CourierIsBusy => new(
+        $"{nameof(Courier).ToLowerInvariant()}.courier.already.is.busy",
+        "Курьер уже занят"
+    );
+}
---
+++ DeliveryApp.Core/Domain/Model/OrderAggregate/Order.cs
@@ -0,0 +1,59 @@
+using CSharpFunctionalExtensions;
+using DeliveryApp.Core.Domain.Model.CourierAggregate;
+using DeliveryApp.Core.Domain.Model.SharedKernel;
+using Primitives;
+
+namespace DeliveryApp.Core.Domain.Model.OrderAggregate;
+
+public class Order : Aggregate<Guid>
+{
+    public Guid? CourierId { get; private set; }
+    public OrderStatus Status { get; private set; }
+    public Location Location { get; private set; }
+
+    private Order(Guid orderId, Location location) : base(orderId)
+    {
+        Location = location;
+    }
+
+    public static Result<Order, Error> Create(Guid orderId, Location location)
+    {
+        if (orderId == Guid.Empty) return GeneralErrors.ValueIsRequired(nameof(orderId));
+        if (location is null) return GeneralErrors.ValueIsRequired(nameof(location));
+        
+        return new Order(orderId, location);
+    }
+
+    public UnitResult<Error> Assign(Courier courier)
+    {
+        if (courier is null) return GeneralErrors.ValueIsRequired(nameof(courier));
+        if (Status != OrderStatus.Created) return Errors.CannotAssignCourierForNotCreatedOrderStatus;
+        
+        CourierId = courier.Id;
+        Status = OrderStatus.Assigned;
+        
+        return UnitResult.Success<Error>();
+    }
+
+    public UnitResult<Error> Complete()
+    {
+        if (Status != OrderStatus.Assigned) return Errors.CannotCompleteNotAssignedOrder;
+        
+        Status = OrderStatus.Completed;
+        
+        return UnitResult.Success<Error>();
+    }
+}
+
+public static class Errors
+{
+    public static Error CannotAssignCourierForNotCreatedOrderStatus => new(
+        $"{nameof(Order).ToLowerInvariant()}.cannot.assign.courier.for.not.created.order.status",
+        $"Нельзя назначить курьера на заказ со статусом не {OrderStatus.Created.Name}"
+    );
+    
+    public static Error CannotCompleteNotAssignedOrder => new(
+        $"{nameof(Order).ToLowerInvariant()}.cannot.complete.not.assigned.order",
+        "Нельзя завершить не назначенный на курьера заказ"
+    );
+}
